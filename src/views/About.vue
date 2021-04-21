37f59ed39a5f8252e7a42a4d8b7565ce451667d3
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-04-21 08:59:25 +0100

    Boilerplate code

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="lg:w-2/5 w-full">
-    <div class="text-center space-y-2 mb-8">
-About</div>
+    <div class="text-center space-y-2 mb-8">About</div>
   </div>
 </template>
 
-<script
-</script>
+<script></script>
 
 <style></style>
